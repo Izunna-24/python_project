e25e9631a349336d5e5b38c46e2a191f67ea1e97
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-04 17:13:23 +0100

    anything

---
+++ class_work/dictionary_play.py
@@ -0,0 +1,4 @@
+sentence_det = {"the": "a", "palace": "b", "is": "c",
+                "the": "d", "palace": "e", "is": "f", "few": "g", "miles": "h", "away": "i", "from": "j", "the": "k",
+                "village": "l", "but": "m", "going": "n", "to": "o", "the": "p", "palace": "q", "to": "r", "see": "s",
+                "startups": "t", "is": "u", "cool": "v", "and": "w", "fun": "x"}
